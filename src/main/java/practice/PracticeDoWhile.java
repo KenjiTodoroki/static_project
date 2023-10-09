83b3c0646fb9ac00aded18737b916b9535022c8d
轟木　賢治 <kenjitodoroki1004@gmail.com>
2023-10-09 16:32:08 +0900

    added PracticeDoWhile

---
+++ src/main/java/practice/PracticeDoWhile.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeDoWhile {
+
+	public static void main(String[] args) {
+		int num = 100; // 初期値
+		
+		do {
+			System.out.println(num);
+			num = num - 1; // 1を引く
+		} while (num >= 0); // numが0になるまで処理を繰り返す
+	}
+
+}
