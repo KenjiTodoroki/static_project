108ea38aa8c33a0eade6c350daaa10b8abb1b2c9
轟木　賢治 <kenjitodoroki1004@gmail.com>
2023-10-09 15:50:45 +0900

    added PracticeWhile

---
+++ src/main/java/practice/PracticeWhile.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class PracticeWhile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num = 100; // 初期値
+		
+		while(num > 0) { // numが0になるまで処理を繰り返す
+			num = num - 1; // 1ずつ引く
+			System.out.println(num); // 変数numを表示
+		}
+	}
+
+}
