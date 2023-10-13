5d9c565820690afb6a9610ee0bf4790b111c707c
轟木　賢治 <kenjitodoroki1004@gmail.com>
2023-10-13 18:44:19 +0900

    fixed PracticeWhile

--- src/main/java/practice/PracticeWhile.java
+++ src/main/java/practice/PracticeWhile.java
@@ -5,11 +5,9 @@ public class PracticeWhile {
 	public static void main(String[] args) {
 		int num = 100; // 初期値
 
-		System.out.println(num); // 100を表示 
-
-		while (num > 0) { // numが0になるまで処理を繰り返す
-			num--; // 1ずつ引く
+		while (num >= 0) { // numが0になるまで処理を繰り返す
 			System.out.println(num); // 変数numを表示
+			num--; // 1ずつ引く
 		}
 	}
 	
